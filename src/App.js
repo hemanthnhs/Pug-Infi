8ba9038f1590d1eb49125568138f24b35134cd3c
Hemanth Nhs <hemanthsai10@gmail.com>
2019-09-21 20:29:17 -0400

    Display of images - without scroll

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            pictures: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('/bomb?count=10')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({pictures: data["pugs"]})
+            })
+            .catch(console.log)
+    }
+
+    render() {
+        var namesList = this.state.pictures.map(function(picture){
+            return <img src={picture} className="Pug-img" alt="Pug Image" />
+        })
+
+        return (
+            <div id = "img-container">
+                {namesList}
+            </div>
+        );
+    }
 }
 
 export default App;
