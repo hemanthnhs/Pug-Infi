8ea67ded3302a0ca0e26080abab43018337dbae7
Hemanth Nhs <hemanthsai10@gmail.com>
2019-09-21 23:13:36 -0400

    Added jquery, Added fetch new pugs data and appending the new images to pictures

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
 import './App.css';
+
 
 class App extends Component {
 
@@ -10,24 +12,44 @@ class App extends Component {
         };
     }
 
-    componentDidMount() {
+    fetchPugs() {
         fetch('/bomb?count=10')
             .then(res => res.json())
             .then((data) => {
-                this.setState({pictures: data["pugs"]})
+                this.setState({pictures: [...this.state.pictures, ...data["pugs"]]})
             })
             .catch(console.log)
     }
 
+    fetchNewData() {
+        this.fetchPugs();
+        console.log("hola");
+    };
+
+    componentDidMount() {
+        this.fetchPugs();
+        window.addEventListener("scroll", (e) => this.scrollEvent(e));
+    }
+
+    scrollEvent() {
+        // Attribution - Reference from Bits and Pieces article
+        var lastLi = document.querySelector("ul#img-container > li:last-child");
+        var lastLiOffset = lastLi.offsetTop + lastLi.clientHeight;
+        var pageOffset = window.pageYOffset + window.innerHeight;
+        if (pageOffset > lastLiOffset) {
+            this.fetchNewData();
+        }
+    }
+
     render() {
-        var namesList = this.state.pictures.map(function(picture){
-            return <img src={picture} className="Pug-img" alt="Pug Image" />
+        var picsList = this.state.pictures.map(function (picture) {
+            return <li><img src={picture} className="Pug-img" alt="Pug Image"/></li>
         })
 
         return (
-            <div id = "img-container">
-                {namesList}
-            </div>
+            <ul id="img-container">
+                {picsList}
+            </ul>
         );
     }
 }
