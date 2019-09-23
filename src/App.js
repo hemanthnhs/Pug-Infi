a3bdeb43adc7964b0333ccee7f7e6c96973a7aad
Hemanth Nhs <hemanthsai10@gmail.com>
2019-09-22 20:45:18 -0400

    OnClick basic modal behaviour implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import $ from 'jquery';
 import './App.css';
+import Modal from "./Modal";
 
 
 class App extends Component {
@@ -10,6 +11,8 @@ class App extends Component {
         this.state = {
             width: 10,
             height: 10,
+            show: false,
+            displayImg: null,
             pictures: [],
         };
     }
@@ -23,11 +26,17 @@ class App extends Component {
             .catch(console.log)
     }
 
+    setWrapperRef(node) {
+        this.wrapperRef = node;
+    }
+
     componentDidMount() {
         this.fetchPugs();
         this.setState({width: window.innerWidth, height: window.innerHeight});
         window.addEventListener("scroll", (e) => this.scrollEvent(e));
         window.addEventListener("resize", (e) => this.windowResize(e));
+        window.addEventListener('mousedown', this.handleClickOutside);
+
     }
 
     windowResize() {
@@ -44,6 +53,21 @@ class App extends Component {
         }
     }
 
+    handleClickOutside = (event) => {
+        if (event.target.tagName == 'DIV') {
+            this.hideModal()
+        }
+    }
+
+    showModal = (ev) => {
+        this.setState({show: true, displayImg: ev.target});
+        console.log(ev.target)
+    };
+
+    hideModal = () => {
+        this.setState({show: false});
+    };
+
     render() {
         var that = this;
         var picsList = this.state.pictures.map(function (picture, index) {
@@ -51,13 +75,19 @@ class App extends Component {
                                         style={{
                                             maxHeight: 0.65 * that.state.height,
                                             maxWidth: 0.9 * that.state.width
-                                        }}/></li>
+                                        }} onClick={that.showModal}
+            /></li>
         })
 
         return (
-            <ul id="img-container">
-                {picsList}
-            </ul>
+            <div>
+                <ul id="img-container">
+                    {picsList}
+                </ul>
+                <div id={this.state.show ? 'modal-container' : ''} ref={this.setWrapperRef()}>
+                    <Modal show={this.state.show} img={this.state.displayImg}/>
+                </div>
+            </div>
         );
     }
 }
---
+++ src/Modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Modal.css";
+
+export default class Modal extends React.Component {
+    render() {
+        if (!this.props.show) {
+            return null;
+        }
+
+        return <div id="modal"><img id="img-popup" src={this.props.img.src}/></div>;
+    }
+}
